608f2cf417f673d07b5c4d833dbe07c51378ba51
Tim Williams <tim@teachmatic.com>
2014-12-20 20:12:23 +0000

    formatting

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -26,103 +26,101 @@ public class Application extends Controller {
       render();
     }
 
-    public static String protectedDBTest(){
-        // access "default" database
-   Connection conn = null;
-   Statement stmt = null;
-   try{
-      //STEP 2: Register JDBC driver
-      Class.forName("org.postgresql.Driver");
-      String database = "d50vt48u98fijd";
-      String DB_URL="jdbc:postgresql://54.235.245.180:5432/"+database+"?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory";
-    String USER = "lvrdcwczyzpzjb";
-    String PASS="y1dkkfMg9WP2GA9z1a3GFLSvB9";
-      //STEP 3: Open a connection
-      System.out.println("Connecting to database...");
-      conn = DriverManager.getConnection(DB_URL,USER,PASS);
-      //STEP 4: Execute a query
-      stmt = conn.createStatement();
-      String sql;
-      sql = " SELECT client_addr FROM pg_stat_activity where client_addr is not null and datname='"+database+"' limit 10;";
-      ResultSet rs = stmt.executeQuery(sql);
-       while(rs.next()){
-         //Retrieve by column name
-         String res = rs.getString("client_addr");
-         System.out.println("Result from PROTECTED DB is: "+res);
-      }
-     rs.close();
-      stmt.close();
-      conn.close();
-   }catch(SQLException se){
-      //Handle errors for JDBC
-      se.printStackTrace();
-   }catch(Exception e){
-      //Handle errors for Class.forName
-      e.printStackTrace();
-   }finally{
-      //finally block used to close resources
-      try{
-         if(stmt!=null)
-            stmt.close();
-      }catch(SQLException se2){
-      }// nothing we can do
-      try{
-         if(conn!=null)
-            conn.close();
-      }catch(SQLException se){
-         se.printStackTrace();
-      }//end finally try
-   }//end try
-    return "";
+    public static String protectedDBTest(){      
+     Connection conn = null;
+     Statement stmt = null;
+     try{
+        //STEP 2: Register JDBC driver
+        Class.forName("org.postgresql.Driver");
+        String database = "database";
+        String DB_URL="jdbc:postgresql://host:port/"+database+"?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory";
+        String USER = "username";
+        String PASS="password";
+        //STEP 3: Open a connection
+        System.out.println("Connecting to database...");
+        conn = DriverManager.getConnection(DB_URL,USER,PASS);
+        //STEP 4: Execute a query
+        stmt = conn.createStatement();
+        String sql;
+        sql = " SELECT client_addr FROM pg_stat_activity where client_addr is not null and datname='"+database+"' limit 10;";
+        ResultSet rs = stmt.executeQuery(sql);
+         while(rs.next()){
+           //Retrieve by column name
+           String res = rs.getString("client_addr");
+           System.out.println("Result from PROTECTED DB is: "+res);
+        }
+       rs.close();
+        stmt.close();
+        conn.close();
+       }catch(SQLException se){
+          //Handle errors for JDBC
+          se.printStackTrace();
+       }catch(Exception e){
+          //Handle errors for Class.forName
+          e.printStackTrace();
+       }finally{
+          //finally block used to close resources
+          try{
+             if(stmt!=null)
+                stmt.close();
+          }catch(SQLException se2){
+          }// nothing we can do
+          try{
+             if(conn!=null)
+                conn.close();
+          }catch(SQLException se){
+             se.printStackTrace();
+          }//end finally try
+       }//end try
+      return "";
     }
 
-    public static String publicDBTest(){
-// access "default" database
-   Connection conn = null;
-   Statement stmt = null;
-   try{
-      //STEP 2: Register JDBC driver
-      Class.forName("org.postgresql.Driver");
-      String database="d34k8b4beftec0";
-      String DB_URL="jdbc:postgresql://54.204.44.31:5432/"+database+"?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory";
-    String USER = "wqqopaejlphvhi";
-    String PASS="uuVUTBzLqCbcyiSIcILPWUhztZ";
-      //STEP 3: Open a connection
-      System.out.println("Connecting to database...");
-      conn = DriverManager.getConnection(DB_URL,USER,PASS);
-      //STEP 4: Execute a query
-      stmt = conn.createStatement();
-      String sql;
-      sql = "SELECT client_addr FROM pg_stat_activity where client_addr is not null and datname='"+database+"' limit 10;";
-      ResultSet rs = stmt.executeQuery(sql);
-       while(rs.next()){
-         //Retrieve by column name
-         String res = rs.getString("client_addr");
-         System.out.println("Result from PUBLIC DB is: "+res);
-      }
-     rs.close();
-      stmt.close();
-      conn.close();
-   }catch(SQLException se){
-      //Handle errors for JDBC
-      se.printStackTrace();
-   }catch(Exception e){
-      //Handle errors for Class.forName
-      e.printStackTrace();
-   }finally{
-      //finally block used to close resources
+    public static String publicDBTest({
+      Connection conn = null;
+      Statement stmt = null;
       try{
-         if(stmt!=null)
-            stmt.close();
-      }catch(SQLException se2){
-      }// nothing we can do
-      try{
-         if(conn!=null)
-            conn.close();
-      }catch(SQLException se){
-         se.printStackTrace();
-      }//end finally try
-   }//end try
+        //STEP 2: Register JDBC driver
+        Class.forName("org.postgresql.Driver");
+        String database="database";
+        String DB_URL="jdbc:postgresql://host:5432/"+database+"?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory";
+        String USER = "username";
+        String PASS="password";
+        //STEP 3: Open a connection
+        System.out.println("Connecting to database...");
+        conn = DriverManager.getConnection(DB_URL,USER,PASS);
+        //STEP 4: Execute a query
+        stmt = conn.createStatement();
+        String sql;
+        sql = "SELECT client_addr FROM pg_stat_activity where client_addr is not null and datname='"+database+"' limit 10;";
+        ResultSet rs = stmt.executeQuery(sql);
+         while(rs.next()){
+           //Retrieve by column name
+           String res = rs.getString("client_addr");
+           System.out.println("Result from PUBLIC DB is: "+res);
+        }
+       rs.close();
+        stmt.close();
+        conn.close();
+       }catch(SQLException se){
+          //Handle errors for JDBC
+          se.printStackTrace();
+       }catch(Exception e){
+          //Handle errors for Class.forName
+          e.printStackTrace();
+       }finally{
+          //finally block used to close resources
+          try{
+             if(stmt!=null)
+                stmt.close();
+          }catch(SQLException se2){
+          }// nothing we can do
+          try{
+             if(conn!=null)
+                conn.close();
+          }catch(SQLException se){
+             se.printStackTrace();
+          }//end finally try
+       }//end try
    return "";
     }
 
